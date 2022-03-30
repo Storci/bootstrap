227abad0c1cdb64c2dec95f97f77fffd63af4b68
simonezardi <simone.zardi@storci.com>
2022-03-30 13:03:06 +0200

    commit 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,12 @@ import * as tw from "./Global/Thingworx/thingworx_api_module.js";
 // definisce l'url di base della pagina attuale (in questo caso della pagina index.html).
 // il risultato è http(s)://xxx.xxx.xxx.xxx:xxxx
 // baseURL verrà utilizzato come base per il cambio pagina.
-let baseURL = window.location.protocol + "//" + window.location.host;
-let pageURL = window.location.href
-console.log(window.location.href)
-console.log(window.location.hostname)
-console.log(window.location.pathname)
-console.log(window.location.protocol)
-console.log(window.location.assign())
+let baseURL = window.location.protocol + "//" + window.location.host
+if(window.location.protocol == 'https:'){
+	baseURL = window.location.protocol + "//" + window.location.host + '/bootstrap'
+}
 
-console.log(baseURL)
-console.log(pageURL)
+let pageURL = window.location.href
 
 // Recupera il nome dell'utente da firebase, controlla che sia loggato.
 // Nel caso non fosse loggato richiama la pagina di login
